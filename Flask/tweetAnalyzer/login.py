98d3796035d51ca99386c393d64f1e9c5950c2f2
kggiorno <kggiorno@gmail.com>
2016-12-30 22:22:17 +0100

    fixed initial tweetAnalyzer

--- Flask/tweetAnalyzer/login.py
+++ Flask/tweetAnalyzer/login.py
@@ -1,18 +1,19 @@
-import constants
+import settings
 import oauth2
 import urllib.parse as urlparse
+import json
 
-consumer = oauth2.Consumer(constants.CONSUMER_KEY, constants.CONSUMER_SECRET)
+consumer = oauth2.Consumer(settings.CONSUMER_KEY, settings.CONSUMER_SECRET)
 client = oauth2.Client(consumer)
 
-response, content = client.request(constants.REQUEST_TOKEN_URL, 'POST')
+response, content = client.request(settings.REQUEST_TOKEN_URL, 'POST')
 if response.status != 200:
 	print("An error occurred during request token retrieval attempt.")
 
 request_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
 print("Navigate to the following site to retrieve the authorization pin:")
-print("{}?oauth_token={}".format(constants.AUTHORIZATION_URL, request_token['oauth_token']))
+print("{}?oauth_token={}".format(settings.AUTHORIZATION_URL, request_token['oauth_token']))
 
 oauth_verifier = input("Enter the authorization pin: ")
 
@@ -20,7 +21,21 @@ token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_se
 token.set_verifier(oauth_verifier)
 client = oauth2.Client(consumer, token)
 
-response, content = client.request(constants.ACCESS_TOKEN_URL, 'POST')
+response, content = client.request(settings.ACCESS_TOKEN_URL, 'POST')
 access_token = dict(urlparse.parse_qsl(content.decode('utf-8')))
 
 print(access_token)
+
+authorized_token = oauth2.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
+authorized_client = oauth2.Client(consumer, authorized_token)
+
+response, content = authorized_client.request('https://api.twitter.com/1.1/search/tweets.json?q=computers+filter:images', 'GET')
+if response.status != 200:
+	print("An error occured during the search API request.")
+
+print(content.decode('utf-8'))
+
+tweets = json.loads(content.decode('utf-8'))
+
+for tweet in tweets['statuses']:
+	print(tweet['text'])
